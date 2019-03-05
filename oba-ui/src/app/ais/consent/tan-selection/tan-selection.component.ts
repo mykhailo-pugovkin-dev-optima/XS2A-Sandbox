27bc14fa7ab3a466807bd348f0ef29252efcca7b
Vladimir Nechai <vne@adorsys.de>
2019-03-05 23:06:16 +0100

    tan selection template

--- oba-ui/src/app/ais/consent/tan-selection/tan-selection.component.ts
+++ oba-ui/src/app/ais/consent/tan-selection/tan-selection.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Account} from "../../../models/account";
 import {Balance} from "../../../models/balance";
-import {AccountConsent} from "../../../../../../online-banking-ui/src/app/model/aspsp/accountConsent";
+import BalanceTypeEnum = Balance.BalanceTypeEnum;
+import {Amount} from "../../../models/amount";
 
 @Component({
   selector: 'app-tan-selection',
@@ -10,17 +11,54 @@ import {AccountConsent} from "../../../../../../online-banking-ui/src/app/model/
 })
 export class TanSelectionComponent implements OnInit {
 
-  accounts: Account[];
-  selectedAccounts = new Array<Account>();
-  consent: AccountConsent;
+  accounts: Account[] = [];
+  consentValidTo = new Date('2019-05-01');
 
   constructor() { }
 
   ngOnInit() {
+    this.getAccounts();
   }
 
   private getAccounts() {
-   let balance1 = new Balance();
+    let amount1:Amount = {amount: '20', currency: 'EUR'};
+    let balance1:Balance = {balanceAmount: amount1, balanceType: BalanceTypeEnum.AVAILABLE};
+    let account1:Account = {
+      id: '1234576',
+      iban: 'DE2783782783283782',
+      bban: '',
+      currency: 'EUR',
+      name: 'My example account',
+      accountType: 'Example type',
+      balances: [balance1],
+      _links: {
+        viewBalances: 'link2balance',
+        viewTransactions: 'link2transactions'
+      }
+    };
+
+    let amount2:Amount = {amount: '400', currency: 'EUR'};
+    let balance2:Balance = {balanceAmount: amount2, balanceType: BalanceTypeEnum.AVAILABLE};
+    let account2:Account = {
+      id: '9866535',
+      iban: 'DE9783782783284646',
+      bban: '',
+      currency: 'EUR',
+      name: 'My second example account',
+      accountType: 'Example type',
+      balances: [balance2],
+      _links: {
+        viewBalances: 'link2balance',
+        viewTransactions: 'link2transactions'
+      }
+    };
+
+    this.accounts.push(account1);
+    this.accounts.push(account2);
+  }
+
+  private getConsent() {
+
   }
 
 }
