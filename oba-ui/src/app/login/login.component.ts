faa3d58231a58b25d2939c74a37956df9fb65e73
Hergy Tchuinkou <htc@adorsys.de>
2019-03-06 15:18:15 +0100

    generated services and models from swagger

--- oba-ui/src/app/login/login.component.ts
+++ oba-ui/src/app/login/login.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {first} from "rxjs/operators";
+import {PSUAISService} from "../../../services/psuais.service";
 
 @Component({
-  selector: 'app-login',
+  selector: 'ais-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+//  submitted = false;
+  returnUrl: string;
+  error: string;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private route: ActivatedRoute,
+              private psuAisService: PSUAISService) {
+  }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: ['', Validators.required],
+      pin: ['', Validators.required]
+    });
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    this.onSubmit();
+  }
+
+  onSubmit() {
+
+    //check when submitting credentials for login
+ //   this.submitted = true;
+
+    //check while authenticate and make post for login
+    this.loading = true;
+    this.psuAisService.loginUsingPOST((this.loginForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }));
   }
 
 }
