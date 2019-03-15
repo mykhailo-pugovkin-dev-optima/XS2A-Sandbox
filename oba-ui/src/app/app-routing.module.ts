27ad34e8ac3a9e1c40e433e76f0d75ef46e2d51f
Vladimir Nechai <vne@adorsys.de>
2019-03-15 14:21:36 +0100

    auth guard

--- oba-ui/src/app/app-routing.module.ts
+++ oba-ui/src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {RoutingPath} from './common/models/routing-path.model';
 import {LoginComponent} from './login/login.component';
--- oba-ui/src/app/common/guards/auth-guard.service.ts
+++ oba-ui/src/app/common/guards/auth-guard.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import {CanActivate} from "@angular/router";
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor() { }
+  constructor(private cookieService: CookieService) { }
 
   // canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
   //   return undefined;
   // }
 
   canActivate() {
-    return true;
+    if (this.cookieService.get('ACCESS_TOKEN')) {
+      return true;
+    }
+
+    return false;
   }
 }
